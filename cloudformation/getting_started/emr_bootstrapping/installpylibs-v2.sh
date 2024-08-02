e7fc38bab8f0b565119e079029afd6cb96222993
thesullivantage <38443606+thesullivantage@users.noreply.github.com>
2024-08-02 02:45:40 -0400

    added some dependencies to installpylibs-v2.sh

--- cloudformation/getting_started/emr_bootstrapping/installpylibs-v2.sh
+++ cloudformation/getting_started/emr_bootstrapping/installpylibs-v2.sh
@@ -15,3 +15,8 @@ sudo /usr/bin/python3 -m pip install -U matplotlib==3.4.3
 sudo /usr/bin/python3 -m pip install -U requests==2.26.0 
 sudo /usr/bin/python3 -m pip install -U boto3==1.18.63 
 sudo /usr/bin/python3 -m pip install -U pandas==1.2.5 
+
+# Custom Dependencies added here:
+sudo /usr/bin/python3 -m pip install nltk
+sudo /usr/bin/python3 -m pip install spark-nlp
+
